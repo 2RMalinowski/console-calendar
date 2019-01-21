d09ac8018244f58d6de06985a9e5e71011669bd8
malin4 <robbmalinowski@outlook.com>
2019-01-21 18:11:20 +0100

    initial commit

---
+++ calendar.py
@@ -0,0 +1,7 @@
+"""
+You should not use any global variables. If you need to store state (for
+example, a list of meetings), you will need to pass it to the functions that
+need it.
+"""
+
+# Milestone 1: Schedule and cancel
