f55dad23c3991294e294686c728b5574a0d35b4e
malin4 <robbmalinowski@outlook.com>
2019-01-22 14:29:30 +0100

    display_schedule() refactored

--- ui.py
+++ ui.py
@@ -5,14 +5,19 @@ def get_input(message):
 
 
 #  displaying functions
-def display_elements_of(any_list):
-    for element in any_list:
-        print(element)
+def display_schedule(meeting_list):
+    print('Your schedule for the day :')
+    for element in meeting_list:
+        print(' '.join(element))
 
 
 def display_program_menu(menu_commands):
     for option in menu_commands:
         print(f'({option[0]}) {option}')
+
+
+def display_message(message):
+    print(message)
 
 
 def display_error_message(message):
