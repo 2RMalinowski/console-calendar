ac47cb38b45dc24d6fca1969e7d6b6485d3c7b52
malin4 <robbmalinowski@outlook.com>
2019-01-22 13:07:19 +0100

    handle_menu() and main() implemented

--- calendar.py
+++ calendar.py
@@ -4,4 +4,26 @@ example, a list of meetings), you will need to pass it to the functions that
 need it.
 """
 
-# Milestone 1: Schedule and cancel
+import ui
+import storage
+import modify
+
+
+def handle_menu():
+    menu_commands = ['schedule a new meeting',
+                     'cancel an existing meeting',
+                     'quit']
+    ui.display_program_menu(menu_commands)
+
+
+def main():
+    while True:
+        handle_menu()
+        try:
+            choose_options_menu()
+        except ValueError as error:
+            ui.display_error_message(error)
+
+
+if __name__ == '__main__':
+    main()
