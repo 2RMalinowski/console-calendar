cd40e12b4fd3fa845db9966123e30f9f0d500f1e
malin4 <robbmalinowski@outlook.com>
2019-01-23 11:35:43 +0100

    redundant returns removed

--- modify.py
+++ modify.py
@@ -6,16 +6,14 @@ meeting_list = []
 def add_new_meeting_to(meeting_list):
     new_meeting = [ui.title('Enter meeting title: '),
                    ui.duration('Enter duration in hours (1 or 2): '),
-                   ui.get_input('Enter start time: ')]
+                   ui.start_time('Enter start time: ')]
     meeting_list.append(new_meeting)
     ui.display_message('Meeting added')
-    return meeting_list
 
 
 def cancel_meeting_in(meeting_list):
-    start_time = ui.get_input('Enter the start time: ')
+    start_time = input('Enter the start time: ')
     for meeting in meeting_list:
         if start_time in meeting:
             meeting_list.remove(meeting)
             ui.display_message('Meeting canceled')
-            return meeting_list
